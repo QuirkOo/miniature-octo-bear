faa8db016b9db4ac69400e9a687505404719f458
Beata Obrok <beata@obrok.eu>
2015-02-23 23:14:04 +0100

    Base express config and routes

---
+++ server.js
@@ -0,0 +1,26 @@
+
+var express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose');
+
+var app = express();
+
+// Views
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./app/routes/core'));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.listen(3000);
+
+module.exports = app;
