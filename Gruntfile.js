73c5fe14d264ffee9f07476971f8afb70e7c9f90
Beata Obrok <beata@obrok.eu>
2015-02-24 01:20:02 +0100

    Add grunt tasks for compiling, serving and watching files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,63 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    express: {
+      options: {
+        script: 'server.js'
+      },
+      default: {
+        node_env: 'dev'
+      }
+    },
+    watch: {
+      js: {
+        files: [ 'Gruntfile.js' ],
+        tasks: [ 'jshint' ],
+        options: {
+          livereload: true
+        }
+      },
+      sass: {
+        files: [ 'app/assets/sass/**/*.sass' ],
+        tasks: [ 'sass' ]
+      },
+      css: {
+        files: [ 'pubic/assets/css/**/*.css' ],
+        options: {
+          livereload: true
+        }
+      },
+      jade: {
+        files: [ 'app/**/*.jade' ],
+        options: {
+          livereload: true
+        }
+      }
+    },
     jshint: {
       all: [ 'Gruntfile.js' ]
+    },
+    sass: {
+      options: {
+        noCache: true,
+        sourcemap: 'none'
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'app/assets/sass',
+          src: [ '*.sass' ],
+          dest: 'public/assets/css',
+          ext: '.css'
+        }]
+      }
     }
   });
 
+  grunt.registerTask('dist', [ 'sass' ]);
+
   grunt.registerTask('test', [ 'jshint' ]);
 
-  grunt.registerTask('default', [ 'test' ]);
+  grunt.registerTask('default', [ 'test', 'dist', 'express', 'watch' ]);
 
 };
