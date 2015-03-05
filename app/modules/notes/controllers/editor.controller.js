ad45189ec80e2613c7951764cd57f7e7d31c2655
Quirk0o <beata@obrok.eu>
2015-03-05 21:01:28 +0100

    Cosmetic changes

--- app/modules/notes/controllers/editor.controller.js
+++ app/modules/notes/controllers/editor.controller.js
@@ -1,21 +1,20 @@
 
-var app = angular.module('NoteApp');
+angular.module('NoteApp')
+    .controller('EditorController', [ '$scope', function($scope) {
 
-app.controller('EditorController', [ '$scope', function($scope) {
+      $scope.aceLoaded = function(editor) {
+        // console.log('Ace loaded');
+        editor.getSession().setMode('ace/mode/markdown');
 
-  $scope.aceLoaded = function(editor) {
-    console.log('Ace loaded');
-    editor.getSession().setMode('ace/mode/markdown');
+        // focus the ace editor
+        editor.focus();
+        var session = editor.getSession();
+        var count = session.getLength();
+        //Go to end of the last line
+        editor.gotoLine(count, session.getLine(count-1).length);
+      };
 
-    //To focus the ace editor
-    editor.focus();
-    var session = editor.getSession();
-    var count = session.getLength();
-    //Go to end of the last line
-    editor.gotoLine(count, session.getLine(count-1).length);
-  };
+      $scope.text = '';
 
-  $scope.text = '';
-
-}]);
+    }]);
 
