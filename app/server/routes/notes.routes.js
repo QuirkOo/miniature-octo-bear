3d202369365a3aabc2add31879df75fcc7bf2957
Quirk0o <beata@obrok.eu>
2015-03-06 21:09:52 +0100

    Add routes for updating and deleting notes

--- app/server/routes/notes.routes.js
+++ app/server/routes/notes.routes.js
@@ -11,7 +11,6 @@ router.get('/', function(req, res, next) {
     }
     res.json(notes);
   });
-
 });
 
 router.post('/', function(req, res, next) {
@@ -22,12 +21,31 @@ router.post('/', function(req, res, next) {
     }
     res.json(note);
   });
-
 });
 
 router.get('/:id', function(req, res, next) {
 
   Note.findById(req.params.id, function(err, note) {
+    if (err) {
+      return next(err);
+    }
+    res.json(note);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+
+  Note.findByIdAndUpdate(req.params.id, req.body, function(err, note) {
+    if (err) {
+      return next(err);
+    }
+    res.json(note);
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+
+  Note.findByIdAndRemove(req.params.id, function(err, note) {
     if (err) {
       return next(err);
     }
